a617d424ed8127fe69f23c08ed6ebad0775a431d
victor-ln <vlima-nu@student.42sp.org.br>
2023-02-14 17:08:51 -0300

    feat: :rocket: adding pair and make_pair finished

---
+++ utility/pair.hpp
@@ -0,0 +1,70 @@
+/* Copyright © 2022 Victor Nunes, Licensed under the MIT License. */
+
+#ifndef UTILITY_PAIR_HPP_
+#define UTILITY_PAIR_HPP_
+
+namespace ft {
+
+template <class T1, class T2>
+struct pair {
+    typedef T1      first_type;
+    typedef T2      second_type;
+
+    T1      first;
+    T2      second;
+
+    pair() : first(), second() {}
+
+    template<class U, class V>
+    pair(const pair<U, V>& pr) : first(pr.first), second(pr.second) {}
+
+    pair(const first_type& a, const second_type& b) : first(a), second(b) {}
+
+    ~pair() {}
+
+    pair& operator= (const pair& pr) {
+        first = pr.first;
+        second = pr.second;
+        return *this;
+    }
+};
+
+template <class T1, class T2>
+inline bool    operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+    return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+template <class T1, class T2>
+inline bool    operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <class T1, class T2>
+inline bool    operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+    return lhs.first < rhs.first || \
+        (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+}
+
+template <class T1, class T2>
+inline bool    operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+    return !(rhs < lhs);
+}
+
+template <class T1, class T2>
+inline bool    operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+    return rhs < lhs;
+}
+
+template <class T1, class T2>
+inline bool    operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+    return !(lhs < rhs);
+}
+
+template <class T1, class T2>
+inline pair<T1, T2> make_pair(T1 x, T2 y) {
+    return pair<T1, T2>(x, y);
+}
+
+}   // namespace ft
+
+#endif  // UTILITY_PAIR_HPP_
