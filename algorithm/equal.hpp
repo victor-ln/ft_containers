fa3c276113e9cd3a2ef996c0d925c4ab0684fd27
victor-ln <vlima-nu@student.42sp.org.br>
2023-02-17 15:18:40 -0300

    feat: :rocket: splitting code into two files

--- algorithm/equal.hpp
+++ algorithm/equal.hpp
@@ -28,36 +28,6 @@ inline bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2,
     return true;
 }
 
-template <class InputIt1, class InputIt2>
-bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
-                             InputIt2 last2) {
-    while (first1 != last1) {
-        if (first2 == last2 || *first2 < *first1) {
-            return false;
-        }
-        if (*first1 < *first2) {
-            return true;
-        }
-        ++first1; ++first2;
-    }
-    return (first2 != last2);
-}
-
-template <class InputIt1, class InputIt2, class Compare>
-bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
-                             InputIt2 last2, Compare comp) {
-    while (first1 != last1) {
-        if (first2 == last2 || comp(*first2, *first1)) {
-            return false;
-        }
-        if (comp(*first1, *first2)) {
-            return true;
-        }
-        ++first1; ++first2;
-    }
-    return (first2 != last2);
-}
-
 }   // namespace ft
 
 #endif  // ALGORITHM_EQUAL_HPP_
---
+++ algorithm/lexicographical_compare.hpp
@@ -0,0 +1,40 @@
+/* Copyright © 2022 Victor Nunes, Licensed under the MIT License. */
+
+#ifndef ALGORITHM_LEXICOGRAPHICAL_COMPARE_HPP_
+#define ALGORITHM_LEXICOGRAPHICAL_COMPARE_HPP_
+
+namespace ft {
+
+template <class InputIt1, class InputIt2>
+bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                             InputIt2 last2) {
+    while (first1 != last1) {
+        if (first2 == last2 || *first2 < *first1) {
+            return false;
+        }
+        if (*first1 < *first2) {
+            return true;
+        }
+        ++first1; ++first2;
+    }
+    return (first2 != last2);
+}
+
+template <class InputIt1, class InputIt2, class Compare>
+bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                             InputIt2 last2, Compare comp) {
+    while (first1 != last1) {
+        if (first2 == last2 || comp(*first2, *first1)) {
+            return false;
+        }
+        if (comp(*first1, *first2)) {
+            return true;
+        }
+        ++first1; ++first2;
+    }
+    return (first2 != last2);
+}
+
+}   // namespace ft
+
+#endif  // ALGORITHM_LEXICOGRAPHICAL_COMPARE_HPP_
