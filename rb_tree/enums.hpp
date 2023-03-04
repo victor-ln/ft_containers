47554588aa03d08000aec9ee32f25812dd314a5d
victor-ln <vlima-nu@student.42sp.org.br>
2023-03-04 18:42:07 -0300

    feat: :rocket: Moving enum definitions to separated file

---
+++ rb_tree/enums.hpp
@@ -0,0 +1,18 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+namespace ft {
+
+enum insertMode {
+    noReplace = false,
+    replace = true
+};
+
+enum nodeColor {
+    black = false,
+    red = true
+};
+    
+}   /* namespace ft */
+
+#endif  /* ENUMS_H */
